f5369c8ea0ad2dd6a308a1bbca831645a716874b
Simon Gaeremynck <gaeremyncks@gmail.com>
2014-10-11 22:56:18 +0100

    First stab at a simple app server

--- README.md
+++ README.md
@@ -2,3 +2,32 @@ grasshopper
 ===========
 
 Grasshopper Event Engine
+
+# Setup
+
+1. Install postgres
+```
+brew install postgresql
+```
+
+2. Create a database and user in postgres
+
+```
+psql template1
+template1=# CREATE USER grasshopper WITH PASSWORD 'grasshopper';
+CREATE ROLE
+template1=# CREATE DATABASE grasshopper;
+CREATE DATABASE
+template1=# GRANT ALL PRIVILEGES ON DATABASE grasshopper TO grasshopper
+```
+
+3. Install the dependencies
+```
+npm install
+```
+
+4. Run the app server
+```
+node app
+```
+
---
+++ config.js
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2014 "Fronteer LTD"
+ * Grasshopper Event Engine
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var bunyan = require('bunyan');
+var PrettyStream = require('bunyan-prettystream');
+
+// The global config object
+var config = module.exports = {};
+
+/*!
+ * The database related configuration
+ *
+ * @see https://github.com/sequelize/sequelize/wiki/API-Reference-Sequelize#new-sequelize
+ */
+config.db = {
+    'database': 'grasshopper',
+    'username': 'grasshopper',
+    'password': 'grasshopper',
+    'dialect': 'postgres',
+    'port': 5432,
+
+    // Disable sequelize logger
+    'logging': function() {}
+};
+
+// Pretty-print the logs to standard out
+var prettyStdOut = new PrettyStream();
+prettyStdOut.pipe(process.stdout);
+
+/*!
+ * The log related configuration
+ */
+config.log = {
+    'streams': [
+        {
+            'level': 'info',
+            'stream': prettyStdOut
+        }
+    ],
+    'serializers': {
+        'err': bunyan.stdSerializers.err,
+        'req': bunyan.stdSerializers.req,
+        'res': bunyan.stdSerializers.res
+    }
+};
+
+/*!
+ * The cookie related configuration
+ */
+config.cookie = {
+    'secret': 'The only reason for time is so that everything doesn\'t happen at once.'
+};
