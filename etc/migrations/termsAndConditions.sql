641db3c1e4568e99705c000cb6aff7a5deebee75
Simon Gaeremynck <gaeremyncks@gmail.com>
2015-06-30 12:46:56 -0700

    Revert "Merge pull request #116 from fronteerio/termsAndConditions"
    This reverts commit 5fa0b516334dc3e0d25510ffb11adee5b3c8e6cd, reversing
    changes made to ba5c7e320e0c6533a8cb549584c7e6b425b3d568.

--- etc/migrations/termsAndConditions.sql
+++
@@ -1,3 +0,0 @@
-alter table "Configs" add "termsAndConditionsText" text default ''::text;
-alter table "Configs" add "termsAndConditionsEnabled" boolean default false;
-alter table "Configs" add "termsAndConditionsLastUpdate" timestamp with time zone;
