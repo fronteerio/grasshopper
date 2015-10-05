865a0a50235d9608299bdf87bcafe9044c4ce063
Simon Gaeremynck <gaeremyncks@gmail.com>
2015-10-05 15:12:30 +0100

    Added three indices to speed up calendar querying

---
+++ etc/migrations/calendar-indices.sql
@@ -0,0 +1,3 @@
+CREATE INDEX idx_eventsseries_serieid ON "EventsSeries" ("SerieId");
+CREATE INDEX idx_calendarseries_userid ON "CalendarSeries" ("CalendarUserId");
+CREATE INDEX idx_event_organisers_eventid ON "Event_Organisers" ("EventId");
